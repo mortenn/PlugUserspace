2cda79c8b0f670db64fc054602dc8db2557bb0e3
Morten Nilsen <morten@runsafe.no>
2015-04-19 04:01:53 +0200

    Versions bumped

--- version.js
+++ version.js
@@ -1,8 +1,8 @@
 (function(){
-	window.freshy.currentVersion('freshy', 13);
-	window.freshy.currentVersion('soundbank', 42);
-	window.freshy.currentVersion('chatalert', 8);
-	window.freshy.currentVersion('noisy', 33);
-	window.freshy.currentVersion('notify', 7);
-	window.freshy.currentVersion('settings', 36);
+	window.freshy.currentVersion('freshy', 25);
+	window.freshy.currentVersion('soundbank', 66);
+	window.freshy.currentVersion('chatalert', 12);
+	window.freshy.currentVersion('noisy', 42);
+	window.freshy.currentVersion('notify', 10);
+	window.freshy.currentVersion('settings', 53);
 })();
