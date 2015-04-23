6d12ad55aa96ccf70799c166432025cbb8b26d3b
Morten Nilsen <morten@runsafe.no>
2015-04-23 23:59:50 +0200

    ..

--- core.js
+++ core.js
@@ -2,7 +2,7 @@
 	window.freshy.currentVersion('freshy', 34);
 	window.freshy.currentVersion('soundbank', 86);
 	window.freshy.currentVersion('chatalert', 23);
-	window.freshy.currentVersion('noisy', 65);
+	window.freshy.currentVersion('noisy', 66);
 	window.freshy.currentVersion('notify', 10);
 	window.freshy.currentVersion('senpai', 21);
 	window.freshy.currentVersion('settings', 64);
