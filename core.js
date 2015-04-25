e78544e92ada807a6648e39f0921055189ec846d
Morten Nilsen <morten@runsafe.no>
2015-04-25 17:21:10 +0200

    Version bump

--- core.js
+++ core.js
@@ -1,10 +1,10 @@
 (function(){
-	window.freshy.currentVersion('freshy', 34);
-	window.freshy.currentVersion('soundbank', 87);
+	window.freshy.currentVersion('freshy', 35);
+	window.freshy.currentVersion('soundbank', 92);
 	window.freshy.currentVersion('chatalert', 23);
 	window.freshy.currentVersion('noisy', 66);
 	window.freshy.currentVersion('notify', 10);
 	window.freshy.currentVersion('settings', 66);
 	window.freshy.currentVersion('senpai', 106);
-	window.freshy.currentVersion('cubicle', 11);
+	window.freshy.currentVersion('cubicle', 14);
 })();
