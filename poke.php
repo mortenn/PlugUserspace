63cfc04f08cda63d0efe6b454a748ca424aee76b
Morten Nilsen <morten@runsafe.no>
2017-01-30 21:53:42 +0100

    Invalid

--- poke.php
+++ poke.php
@@ -2,7 +2,7 @@
 	$secret = file_get_contents('../.github-secret');
 	$data = file_get_contents('php://input');
 	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
-	if($hash != hash_hmac($algo, $rawPost, $hookSecret))
+	if($hash != hash_hmac($algo, $data, $secret))
 	{
 		header('HTTP/1.0 403 Forbidden');
 		die('Invalid secret');
