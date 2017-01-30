3aa448212bc9b2a1070bc965f3a00616572c81c3
Morten Nilsen <morten@runsafe.no>
2017-01-30 21:51:30 +0100

    Verify secret in poke handler

--- poke.php
+++ poke.php
@@ -1,1 +1,10 @@
-<?php echo `git pull 2>&1`;
+<?php
+	$secret = file_get_contents('../.github-secret');
+	$data = file_get_contents('php://input');
+	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+	if($hash != hash_hmac($algo, $rawPost, $hookSecret))
+	{
+		header('HTTP/1.0 403 Forbidden');
+		die('Invalid secret');
+	}
+	echo `git pull 2>&1`;
