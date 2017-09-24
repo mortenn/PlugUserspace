d8dc5b1ff9c4a5faf2d553e039fc6a071f32c0c3
Morten Nilsen <morten@runsafe.no>
2017-09-24 13:44:21 +0200

    Version bumps

--- core.js
+++ core.js
@@ -5,7 +5,7 @@
 	window.freshy.currentVersion('noisy', 72);
 	window.freshy.currentVersion('notify', 12);
 	window.freshy.currentVersion('settings', 72);
-	window.freshy.currentVersion('senpai', 200);
+	window.freshy.currentVersion('senpai', 202);
 	window.freshy.currentVersion('cubicle', 49);
 	window.freshy.currentVersion('babelfish', 43);
 	window.freshy.currentVersion('socialite', 20);
--- staff.js
+++ staff.js
@@ -1,3 +1,3 @@
 (function(){
-	window.freshy.currentVersion('kouhai', 150);
+	window.freshy.currentVersion('kouhai', 151);
 })();
