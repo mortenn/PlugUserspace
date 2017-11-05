3fb8383fb26f8c66190547153e2930ee4509bb84
Morten Nilsen <morten@runsafe.no>
2017-11-05 13:33:22 +0100

    Make tooltips titles

--- core.js
+++ core.js
@@ -5,11 +5,11 @@
 	window.freshy.currentVersion('noisy', 72, API.ROLE.NONE);
 	window.freshy.currentVersion('notify', 12, API.ROLE.NONE);
 	window.freshy.currentVersion('settings', 72, API.ROLE.NONE);
-	window.freshy.currentVersion('senpai', 217, API.ROLE.NONE);
+	window.freshy.currentVersion('senpai', 218, API.ROLE.NONE);
 	window.freshy.currentVersion('cubicle', 49, API.ROLE.NONE);
 	window.freshy.currentVersion('babelfish', 43, API.ROLE.NONE);
 	window.freshy.currentVersion('socialite', 20, API.ROLE.NONE);
-	window.freshy.currentVersion('multipass', 166, API.ROLE.NONE);
+	window.freshy.currentVersion('multipass', 167, API.ROLE.NONE);
 	window.freshy.currentVersion('fastmode', 5, API.ROLE.NONE);
 	window.freshy.currentVersion('linkify', 6, API.ROLE.NONE);
 	window.freshy.currentVersion('emoji', 11, API.ROLE.NONE);
