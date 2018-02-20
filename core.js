a702f3ec65ac10b9c75ac3ed9f562394e3354f6b
Morten Nilsen <morten@runsafe.no>
2018-02-20 19:28:44 +0100

    Bump versions

--- core.js
+++ core.js
@@ -15,5 +15,5 @@
 	window.freshy.currentVersion('linkify', 6, API.ROLE.NONE);
 	window.freshy.currentVersion('emoji', 11, API.ROLE.NONE);
 	window.freshy.currentVersion('chatfix', 23, API.ROLE.NONE);
-	window.freshy.currentVersion('kouhai', 156, API.ROLE.BOUNCER);
+	window.freshy.currentVersion('kouhai', 157, API.ROLE.BOUNCER);
 })();
